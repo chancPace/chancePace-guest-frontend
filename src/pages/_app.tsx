bb6b1e0722ce07d154849e0db818afe94be9ed8d
songyiiii <gksthddl43@daum.net>
2024-10-17 20:09:19 +0900

    Feat: header 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Header from '@/features/Header';
+import '@/styles/globals.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <Header />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+export const theme = {
+    color: {
+        text: '#000000',
+        textDark:'#333333',
+        warning:'#e74c3c',
+    },
+    fontSizes: {
+        xs: '12px', // 매우 작은 폰트
+        sm: '14px', // 작은 폰트
+        md: '16px', // 기본 폰트 크기
+        lg: '18px', // 큰 폰트
+        xl: '24px', // 매우 큰 폰트
+        '2xl': '30px', // 제목 및 강조용
+    },
+    fonts: {
+        main:'GmarketSansMedium',
+    },
+    spacing: {
+        xs: '4px', // 매우 작은 마진/패딩
+        sm: '8px', // 작은 마진/패딩
+        md: '16px', // 기본 마진/패딩
+        lg: '24px', // 큰 마진/패딩
+        xl: '32px', // 매우 큰 마진/패딩
+    },
+    borderRadius: '8px',
+};
