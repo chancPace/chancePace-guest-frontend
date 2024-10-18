ad4b0df1e18bd860cfbe890a852b5e93d68d30b9
songyiiii <gksthddl43@daum.net>
2024-10-18 14:41:47 +0900

    Feat: login컴포넌트 생성
    2024.10.18

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,6 +1,23 @@
+import { postLogin } from '@/pages/api/api';
+import InputField from '../InputField';
 import { LoginStyled } from './styled';
 
-const Login = () => {
-    return <LoginStyled></LoginStyled>;
+interface loginvalues {
+    email: string;
+    password: string;
+}
+
+const LoginForm = () => {
+    const handleLogin = async (values: loginvalues) => {};
+    return (
+        <LoginStyled>
+            <InputField name={'email'} label={'email'} rules={[]}></InputField>
+            <InputField
+                name={'password'}
+                label={'비밀번호'}
+                rules={[{ required: true, message: '비밀번호를 입력해주세요' }]}
+            ></InputField>
+        </LoginStyled>
+    );
 };
-export default Login;
+export default LoginForm;
