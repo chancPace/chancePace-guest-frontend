81fe62122146ec9e9ae150ed284b7064cb8d6580
songyiiii <gksthddl43@daum.net>
2024-10-21 15:11:59 +0900

    Fix: footer디자인
    2024.10.21

--- src/features/Footer/index.tsx
+++ src/features/Footer/index.tsx
@@ -3,6 +3,11 @@ import { useDispatch, UseDispatch } from 'react-redux';
 import { logout } from '@/redux/slices/userSlice';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
+import { CiLocationOn } from 'react-icons/ci';
+import { CiMail } from 'react-icons/ci';
+import { CiTimer } from 'react-icons/ci';
+
+import { CiHeadphones } from 'react-icons/ci';
 
 const Footer = () => {
     const dispatch = useDispatch();
@@ -16,7 +21,40 @@ const Footer = () => {
     };
     return (
         <FooterStyled>
-            <p onClick={handleLogout}>로그아웃</p>
+            <div className="footer1">
+                <p>이용약관</p>
+                <p>개인정보처리방침</p>
+                <p>정산 및 환불정책</p>
+                <p>공간 관리 정책</p>
+            </div>
+            <div className="footer2">
+                <div>
+                    <CiLocationOn />
+                    <p>서울특별시 마포구 도화2길 53</p>
+                </div>
+                <div>
+                    <CiHeadphones />
+                    <p>02-0000-0000</p>
+                </div>
+                <div>
+                    <CiMail />
+                    <p>chancpace@chancepace-official.com</p>
+                </div>
+                <div>
+                    <CiTimer />
+                    <p>월-금 09:00 - 18:00</p>
+                </div>
+            </div>
+            <div className="footer3">
+                <p>주식회사 찬스페이스</p>
+                <p>대표: 윤석찬</p>
+                <p>사업자등록번호: 000-00-00000</p>
+                <p>통신판매업신고번호: 2024-서울마포구-0000</p>
+            </div>
+            <div className="footer4">
+                <p>Copyright ⓒ2024 chancepace All rights reserved. </p>
+                <p onClick={handleLogout}>로그아웃</p>
+            </div>
         </FooterStyled>
     );
 };
--- src/features/Footer/styled.ts
+++ src/features/Footer/styled.ts
@@ -1,7 +1,37 @@
 import styled from 'styled-components';
 
 export const FooterStyled = styled.div`
-    height: 100px;
-    background-color: #171036;
-    color: white;
+    font-size: ${({ theme }) => theme.fontSizes.sm};
+    border-top: 1px solid black;
+    .footer1 {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        > p {
+            margin: 10px;
+            cursor: pointer;
+        }
+    }
+    .footer2 {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        > div {
+            display: flex;
+            margin: 10px;
+        }
+    }
+    .footer3 {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        border-bottom: 1px solid white;
+        > p {
+            margin: 10px;
+        }
+    }
+    .footer4 {
+        display: flex;
+        justify-content: space-between;
+    }
 `;
