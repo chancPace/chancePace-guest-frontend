a99f5195edf4f1b9f122524a9b5025008110f9c4
songyiiii <gksthddl43@daum.net>
2024-10-23 18:38:44 +0900

    Fix: swiper 화살표 수정
    2024.10.23

--- src/features/MainPage/styled.ts
+++ src/features/MainPage/styled.ts
@@ -7,102 +7,110 @@ export const MainStyled = styled.div`
     margin-bottom: 100px;
     margin-top: 50px;
     /* 스와이퍼 화살표 */
-
-
+    .swiper-button-next,
+    .swiper-button-prev {
+        visibility: hidden;
+    }
+    .swiper:hover {
+        cursor: pointer;
+        .swiper-button-next,
+        .swiper-button-prev {
+            visibility: visible;
+            color: white;
+        }
+    }
     .searchbar {
         height: 100px;
         display: flex;
         align-items: center;
     }
-
-
-        .trendingSpot {
-            margin: auto;
-            .trendingSpotTitle {
-                font-weight: bold;
-                color: #363636;
-                /* text-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.23922); */
-                font-size: 35px;
-                margin-left: 10%;
-                z-index: 100000;
-            }
-            .trendingSpotRight {
-                display: flex;
-                justify-content: flex-end;
-                width: 100%;
-                .slider {
-                    width: 90%;
-                }
+    .trendingSpot {
+        margin: auto;
+        .trendingSpotTitle {
+            font-weight: bold;
+            color: #363636;
+            /* text-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.23922); */
+            font-size: 35px;
+            margin-left: 10%;
+            z-index: 100000;
+        }
+        .trendingSpotRight {
+            display: flex;
+            justify-content: flex-end;
+            width: 100%;
+            .slider {
+                width: 90%;
             }
         }
-        .category {
-            width: 100%;
-            margin-top: 100px;
-            .categoryTitle {
-                text-align: center;
-                font-size: ${({ theme }) => theme.fontSizes.xxl};
-                color: #8c73d8;
-            }
-            .categoryList {
-                display: flex;
-                justify-content: center;
-                flex-wrap: wrap;
-            }
+    }
+    .category {
+        width: 100%;
+        margin-top: 100px;
+        .categoryTitle {
+            text-align: center;
+            font-size: ${({ theme }) => theme.fontSizes.xxl};
+            color: #8c73d8;
         }
-        .placeSection {
-            width: 100%;
-            margin-top: 100px;
-            .swiper-button-next,
-            .swiper-button-prev {
-                color: ${({ theme }) => theme.color.textDark};
-            }
-            .swiper-button-next {
-                right: 10px;
-                top: 40%;
-            }
-            .swiper-button-prev {
-                left: 10px;
-                top: 40%;
-            }
-            .itemListTitle {
-                font-size: 35px;
-                margin-bottom: 15px;
-                font-weight: bold;
-                color: #363636;
-            }
-            .buttonBox {
-                margin-top: 20px;
-                width: 100%;
-                text-align: center;
-                .button {
-                    background-color: white;
-                    color: black;
-                    &:hover {
-                        background-color: #8c73d8;
-                        color: white;
-                        border: 0;
-                    }
-                }
-            }
+        .categoryList {
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
         }
-
-        .itemList {
-            place-items: center;
-            width: 100%;
-            display: grid;
-            grid-template-columns: repeat(4, 1fr);
-            gap: 12px;
+    }
+    .placeSection {
+        width: 100%;
+        margin-top: 100px;
+        .swiper-button-next,
+        .swiper-button-prev {
+            color: ${({ theme }) => theme.color.textDark};
+        }
+        .swiper-button-next {
+            right: 10px;
+            top: 40%;
+        }
+        .swiper-button-prev {
+            left: 10px;
+            top: 40%;
         }
         .itemListTitle {
-            font-size: ${({ theme }) => theme.fontSizes.xxl};
+            font-size: 35px;
             margin-bottom: 15px;
             font-weight: bold;
+            color: #363636;
         }
         .buttonBox {
             margin-top: 20px;
             width: 100%;
             text-align: center;
+            .button {
+                background-color: white;
+                color: black;
+                &:hover {
+                    background-color: #8c73d8;
+                    color: white;
+                    border: 0;
+                }
+            }
         }
+    }
+
+    .itemList {
+        place-items: center;
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 12px;
+    }
+    .itemListTitle {
+        font-size: ${({ theme }) => theme.fontSizes.xxl};
+        margin-bottom: 15px;
+        font-weight: bold;
+    }
+    .buttonBox {
+        margin-top: 20px;
+        width: 100%;
+        text-align: center;
+    }
 
     .itemList {
         place-items: center;
