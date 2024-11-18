fcfe876551c97b948c753da5c36c11c1f78a35b1
songyiiii <gksthddl43@daum.net>
2024-11-18 19:55:34 +0900

    Fix: 오타수정
    2024.11.18

--- src/components/WishList/index.tsx
+++ src/components/WishList/index.tsx
@@ -14,7 +14,7 @@ const WishList = ({ x, onRemove }: WishListProps) => {
   if (x.space?.spaceStatus === 'UNAVAILABLE') {
     return null;
   }
-  const router = useRouter();ㅈ
+  const router = useRouter();
   const [isBookmarked, setIsBookmarked] = useState(true);
 
   const toggleWishlist = async (e: React.MouseEvent) => {
